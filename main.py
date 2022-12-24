d2474f85bd70bd7585cc57ff68d0282d643caf2d
Nate <nate@ship.hub>
2022-12-24 14:33:07 +1300

    Commit with remaining days

--- main.py
+++
@@ -1,9 +0,0 @@
-
-with open("input.txt") as f:
-    lines = f.readlines()
-    movements = []
-    for line in lines:
-        x, y = line.replace("\n", "").split(" ")
-        movements.append([x, int(y)]) 
-
-print(movements)
